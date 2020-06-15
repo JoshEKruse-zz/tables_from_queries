46cf1f5be83a60507f6a8a02b2899f4968b53863
Joshua Kruse <Joshua.Kruse@drivetime.com>
2020-06-15 11:08:32 -0700

    added list comprehension

--- parse_export_data.py
+++ parse_export_data.py
@@ -1,23 +1,18 @@
 import csv
 import sys
 
-with open(sys.argv[1], newline='') as csvfile:
-	reader = list(csv.reader(csvfile))
-	tableset = set()
+# open input csv file from system arguments
+queries = list( csv.reader( open( sys.argv[ 1 ], newline='' ) ) )
+tableset = set() # initialize empty set
 
-	index = 0
-	for row in reader :
-		for line in row :
-			line = line.split()
-			for index, word in enumerate(line) :
-				if word == 'FROM' :
-					tableset.add( line[ index+1 ] )
-				if word == 'JOIN' :
-					tableset.add( line[ index+1 ] )
-		
-	#print( tableset )
+# split list of queries into a list of single words
+wordlist = [ word for line in [ line for row in queries for line in row ] for word in line.split() ]
 
-	with open('output_tables.csv', 'w', newline='')  as csvfile :
-		cw = csv.writer(csvfile)
-		for table in list(tableset) :
-			cw.writerow( [table] )
+# if a 'FROM' or 'JOIN' is found, save next word to the set
+for index, word in enumerate( wordlist ) :
+	if word == 'FROM' : tableset.add( wordlist[ index+1 ] )
+	if word == 'JOIN' : tableset.add( wordlist[ index+1 ] )
+
+# for every table found, output to csv file
+output = csv.writer( open( 'output_tables.csv', 'w', newline='' ) )
+for table in list( tableset ) : output.writerow( [ table ] )
