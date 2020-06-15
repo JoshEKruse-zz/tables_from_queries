666503991201370a7b64b88db0dc21eb775d2317
Joshua Kruse <Joshua.Kruse@drivetime.com>
2020-06-15 15:45:51 -0700

    added APPLY statement and comments

--- parse_export_data.py
+++ parse_export_data.py
@@ -5,13 +5,17 @@ import sys
 queries = list( csv.reader( open( sys.argv[ 1 ], newline='' ) ) )
 tableset = set() # initialize empty set
 
-# split list of queries into a list of single words
-wordlist = [ word for line in [ line for row in queries for line in row ] for word in line.split() ]
+# split list of queries into a list of single line
+linelist = [ line for row in queries for line in row ]
+# split list of lines into a list of single words
+wordlist = [ word for line in linelist for word in line.split() ]
 
 # if a 'FROM' or 'JOIN' is found, save next word to the set
 for index, word in enumerate( wordlist ) :
-	if word == 'FROM' : tableset.add( wordlist[ index+1 ] )
-	if word == 'JOIN' : tableset.add( wordlist[ index+1 ] )
+	nextword = word.replace( '[', '' ).replace( ']', '' ) # remove '[',']' from strings
+	if wordlist[ index-1 ].upper() == 'FROM'  and 'sys.' not in nextword : tableset.add( nextword )
+	if wordlist[ index-1 ].upper() == 'JOIN'  and 'sys.' not in nextword : tableset.add( nextword )
+	if wordlist[ index-1 ].upper() == 'APPLY' and 'sys.' not in nextword : tableset.add( nextword )
 
 # for every table found, output to csv file
 output = csv.writer( open( 'output_tables.csv', 'w', newline='' ) )
